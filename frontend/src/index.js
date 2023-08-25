9859604fd1e741e402a527a82bf9ed1383894ef4
Viviane Le <letranviviane@gmail.com>
2023-08-25 15:07:10 -0700

    Finished Auth Me phase 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,50 +1,25 @@
-// frontend/src/index.js
-import React from 'react';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import sessionReducer from "./session";
 
-import './index.css';
+const rootReducer = combineReducers({
+  // add reducer functions here
+  session: sessionReducer,
+});
 
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { restoreCSRF, csrfFetch } from './store/csrf';
+let enhancer;
 
-import configureStore from './store';
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const store = configureStore();
-
-if (process.env.NODE_ENV !== 'production') {
-  restoreCSRF();
-
-  window.csrfFetch = csrfFetch;
-  window.store = store;
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
 
-function Root() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  );
-}
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+export default configureStore;
