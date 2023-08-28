49312963235477bab380d58f6b5677e7de0b250f
Viviane Le <letranviviane@gmail.com>
2023-08-28 10:40:10 -0700

    Finished phase 1 auth me render deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,29 +1,10 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
-
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-//mod5 week15 auth me p0 Restore the XSRF-TOKEN cookie
-    if (process.env.NODE_ENV !== 'production') {
-      router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
-      });
-    }
-  });
-
-// backend/routes/index.js
-// ...
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-// ...
+
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
@@ -31,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -42,11 +23,18 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 module.exports = router;
---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,6 @@
+// import Navigation from "../Navigation";
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSpots } from '../../store/spotsReducer';
+import './AllSpots.css';
