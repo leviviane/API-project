9737e2af3b0a078e583e559d15a1bc73a74c79db
Viviane Le <letranviviane@gmail.com>
2023-08-31 21:37:23 -0700

    Reseeded

--- backend/db/seeders/20230808154630-spotImages.js
+++ backend/db/seeders/20230808154630-spotImages.js
@@ -17,14 +17,44 @@ module.exports = {
         url: 'https://media.discordapp.net/attachments/1126629909419262092/1146222898357481592/hybe.jpg?width=403&height=539'
       },
       {
+        spotId: 1,
+        preview: false,
+        url: 'https://files.slack.com/files-tmb/T03GU501J-F05QJUQLLUB-164a7cb82c/hybe2_720.jpg'
+      },
+      {
+        spotId: 1,
+        preview: false,
+        url: 'https://files.slack.com/files-tmb/T03GU501J-F05QJUQ61K5-79dbd29404/hybe3_720.jpg'
+      },
+      {
         spotId: 2,
         preview: true,
         url: 'https://cdn.discordapp.com/attachments/1126629909419262092/1146222897833181264/jyp.jpg'
       },
       {
+        spotId: 2,
+        preview: false,
+        url: 'https://files.slack.com/files-tmb/T03GU501J-F05QK2NKQF4-02d7f43b30/jyp2_720.png'
+      },
+      {
+        spotId: 2,
+        preview: false,
+        url: 'https://files.slack.com/files-tmb/T03GU501J-F05QG4Z8G8M-1e9cfa0afa/jyp3_720.jpg'
+      },
+      {
         spotId: 3,
         preview: true,
         url: 'https://cdn.discordapp.com/attachments/1126629909419262092/1146222898667847740/aroundus.jpg',
+      },
+      {
+        spotId: 3,
+        preview: false,
+        url: 'https://files.slack.com/files-tmb/T03GU501J-F05R8PN2FJL-7c0bde31fe/aroundus2_720.jpg'
+      },
+      {
+        spotId: 3,
+        preview: false,
+        url: 'https://files.slack.com/files-tmb/T03GU501J-F05QK32V3M0-c036f05433/aroundus3_720.jpg'
       }
     ])
     /**
