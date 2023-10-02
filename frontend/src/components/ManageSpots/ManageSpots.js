4d4f2c0350991862079bb4ed7e95ef33e5f214f9
Viviane Le <letranviviane@gmail.com>
2023-10-02 05:45:57 -0700

    Still styling, pushing to check on render

--- frontend/src/components/ManageSpots/ManageSpots.js
+++ frontend/src/components/ManageSpots/ManageSpots.js
@@ -7,74 +7,72 @@ import { getSpotsByUserThunk } from '../../store/spots';
 import './ManageSpots.css';
 
 function ManageSpots() {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const userSpots = useSelector((state) => state.spots.allSpots);
-    console.log('userSpots:!!!!', userSpots);
-    const listsOfSpots = Object.values(userSpots);
-    console.log('listOfSpots:!!!!', listsOfSpots);
-    const { userId } = useParams();
-    console.log('Manage Spots:', listsOfSpots);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userSpots = useSelector((state) => state.spots.allSpots);
+  const listsOfSpots = Object.values(userSpots);
+  const { userId } = useParams();
 
-    useEffect(() => {
-        dispatch(getSpotsByUserThunk(userId));
-    }, [dispatch, userId]);
+  useEffect(() => {
+    dispatch(getSpotsByUserThunk(userId));
+  }, [dispatch, userId]);
 
-    const newSpot = () => {
-        history.push('/spots/new');
-    };
+  const newSpot = () => {
+    history.push('/spots/new');
+  };
 
-    return (
-        <div className="manage-spots-container">
-            <h1>Manage Your Spots</h1>
-            <div className="create-container">
-                <button className="create-button" onClick={newSpot}>
-                    Create a Spot
-                </button>
-                {listsOfSpots.length > 0 ? (
-                    listsOfSpots.map((spot) => (
-                        <div className="spot-manage" key={spot.id}>
-                            <NavLink to={`/spot/${spot.id}`}>
-                                <img
-                                    src={spot.previewImage}
-                                    className="image-box"
-                                    alt={spot.name}
-                                    name={spot.name}
-                                />
+  return (
+    <div className="manage-spots-container">
+      <h1>Manage Your Spots</h1>
+      <div className="create-container">
+        <button className="create-button" onClick={newSpot}>
+          Create a Spot
+        </button>
+        <div className="spot-list">
+          {listsOfSpots.length > 0 ? (
+            listsOfSpots.map((spot) => (
+              <div className="spot-manage" key={spot.id}>
+                <NavLink to={`/spot/${spot.id}`}>
+                  <img
+                    src={spot.previewImage}
+                    className="image-box"
+                    alt={spot.name}
+                    name={spot.name}
+                  />
+                </NavLink>
+                <div className="update-spot">
+                  <p>{`${spot.city}, ${spot.state}`}</p>
+                  <div className="ratings-container">
+                    <i className="fa-solid fa-star"></i>
+                    <p>{spot.avgRating || 'New'}</p>
+                  </div>
+                </div>
+                <div>
+                  <p>{`$${spot.price} / night`}</p>
+                </div>
+                <div className="update-button-container">
+                    <div style={{ display: 'inline-block' }}>
+                        <NavLink to={`/spots/${spot.id}/edit`}>
+                            <button className="manage-update-button">Update</button>
                             </NavLink>
-                            <div className="update-spot">
-                                <p>{`${spot.city}, ${spot.state}`}</p>
-                                <div className="ratings-container">
-                                    <i className="fa-solid fa-star"></i>
-                                    <p>{spot.avgRating || 'New'}</p>
-                                </div>
                             </div>
-                            <div>
-                                <p>{`$${spot.price} / night`}</p>
+                            <div style={{ display: 'inline-block' }}>
+                                <OpenModalButton
+                                buttonText="Delete"
+                                modalComponent={
+                                <DeleteSpot spotId={spot.id} ownerId={spot.ownerId} />
+                            } />
                             </div>
-                            <div className="update-button-container">
-                                <div>
-                                    <NavLink to={`/spots/${spot.id}/edit`}>
-                                        <button className="update-button">Update</button>
-                                    </NavLink>
-                                </div>
-                                <div>
-                                    <OpenModalButton
-                                        buttonText="Delete"
-                                        modalComponent={
-                                            <DeleteSpot spotId={spot.id} ownerId={spot.ownerId} />
-                                        }
-                                    />
-                                </div>
                             </div>
-                        </div>
-                    ))
-                ) : (
-                    <p>You don't have any spots yet.</p>
-                )}
-            </div>
+              </div>
+            ))
+          ) : (
+            <p>You don't have any spots yet.</p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ManageSpots;
--- frontend/src/components/SingleSpotDetails/SingleSpotDetails.js
+++ frontend/src/components/SingleSpotDetails/SingleSpotDetails.js
@@ -1,92 +1,72 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom/";
-import { getSingleSpotThunk } from "../../store/spots";
-import { getReviewsThunk } from "../../store/reviews";
-import  { SpotReview } from '../SpotsReview/SpotReview';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom/';
+import { getSingleSpotThunk } from '../../store/spots';
+import { getReviewsThunk } from '../../store/reviews';
+import { SpotReview } from '../SpotsReview/SpotReview';
 import './SingleSpotDetails.css';
 
-function SingleSpotDetails () {
-    const dispatch = useDispatch();
-    const spot = useSelector((state) => state.spots.singleSpot)
-    const {spotId} = useParams();
-    // const review = useSelector((state) => state.reviews.reviews)
+function SingleSpotDetails() {
+  const dispatch = useDispatch();
+  const spot = useSelector((state) => state.spots.singleSpot);
+  const { spotId } = useParams();
 
+  useEffect(() => {
+    dispatch(getReviewsThunk(spotId));
+    dispatch(getSingleSpotThunk(spotId));
+  }, [dispatch, spotId]);
 
-    useEffect(() => {
-        dispatch(getReviewsThunk(spotId))
-        dispatch(getSingleSpotThunk(spotId))
-      }, [dispatch, spotId])
+  if (!spot || Object.keys(spot).length === 0) {
+    return null;
+  }
 
-    if (!spot || Object.keys(spot).length === 0) {
-        return null;
-    }
+  const reserveButton = (e) => {
+    e.preventDefault();
+    alert('Feature Coming Soon...');
+  };
 
-    // if (!spot.id) return null;
+  let counter = 1;
 
-    // console.log('spot:', spot)
-
-    // const noPreviewImage = spot.SpotImages.filter(image => !image.preview);
-
-    const reserveButton = (e) => {
-        e.preventDefault();
-        alert('Feature Coming Soon...')
-    };
-
-    // const checkForReviews = (reviews) => {
-    //     if (reviews === 0) {
-    //         return 'New'
-    //     } else if (reviews === 1) {
-    //         return `${reviews} Review`
-    //     } else return `${reviews} Reviews`
-    // }
-
-
-    return (
-        <div id='spots-details-container'>
-            <div className='name-location-container'>
-                <h2>{spot.name}</h2>
-                <h4>{`${spot.city}, ${spot.state}, ${spot.country}`}</h4>
-            </div>
-            <div className='main-image-container'>
-                {/* <div className='image-container'>
-                {noPreviewImage.map(image => (
-                <img key={image.id} src={image.url} alt='main img' />
-                ))}
-                </div> */}
-                <div className='image-container'>
-                {spot.SpotImages.map(image => (
-                <img key={image.id} src={image.url} alt='main img' />
-                ))}
-            </div>
-            </div>
-            <div className='host-container'>
-                <div className='host-container'>
-                    <h3>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h3>
-                    <h4>{spot.description}</h4>
-                </div>
-            </div>
-            <div className='price-reserve-container'>
-                <div className='price-container'>
-                    <h4>${spot.price} </h4>
-                    <h5>night</h5>
-                </div>
-                <div className='reviews-container'>
-                    <h5 className="ratings">
-                        <i className="fa-solid fa-star"></i>{!spot.avgStarRating ? <span>NEW</span> : spot.avgStarRating.toFixed(1)}
-                    </h5>
-                    {spot.numReviews ? '·' : <p></p>}
-                    <h5 className="numReviews">
-                        {spot.numReviews ? `${spot.numReviews} ${spot.numReviews > 1 ? 'reviews' : 'review'}` : <p></p>}
-                    </h5>
-                </div>
-                <button className='reserve-button' onClick={reserveButton}>Reserve</button>
-            </div>
-            <SpotReview />
+  return (
+    <div className='spots-details-container'>
+      <div className='name-location-container'>
+        <h2>{spot.name}</h2>
+        <h4>{`${spot.city}, ${spot.state}, ${spot.country}`}</h4>
+      </div>
+      <div className='spot-images-container'>
+        {spot.SpotImages &&
+          spot.SpotImages.map((image) => (
+            <img src={image.url} key={image.id} className={`image${counter++}`} />
+          ))}
+      </div>
+      <div className='host-container'>
+        <h3>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h3>
+        <div>
+        <h4>{spot.description}</h4>
         </div>
-    )
+      </div>
+      <div className='price-reserve-container'>
+        <div className='flex-right-container'>
+          <div className='price-container'>
+            <h4>${spot.price} </h4>
+            <h5>night</h5>
+          </div>
+          <div className='reviews-container'>
+            <h5 className='ratings'>
+              <i className='fa-solid fa-star'></i>{!spot.avgStarRating ? <span>NEW</span> : spot.avgStarRating.toFixed(1)}
+            </h5>
+            {spot.numReviews ? '·' : <p></p>}
+            <h5 className='numReviews'>
+              {spot.numReviews ? `${spot.numReviews} ${spot.numReviews > 1 ? 'reviews' : 'review'}` : <p></p>}
+            </h5>
+          </div>
+          <button className='reserve-button' onClick={reserveButton}>Reserve</button>
+        </div>
+      </div>
+      <SpotReview />
+    </div>
+  );
 }
-
 
 export default SingleSpotDetails;
 
@@ -94,11 +74,11 @@ export default SingleSpotDetails;
 
 
 
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect } from "react";
-// import { useParams } from "react-router-dom/";
-// import { getSingleSpotThunk } from "../../store/spots";
-// import { getReviewsThunk } from "../../store/reviews";
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useParams } from 'react-router-dom/';
+// import { getSingleSpotThunk } from '../../store/spots';
+// import { getReviewsThunk } from '../../store/reviews';
 // import './SingleSpotDetails.css';
 
 // function SingleSpotDetails () {
@@ -166,11 +146,11 @@ export default SingleSpotDetails;
 //                     <h5>night</h5>
 //                 </div>
 //                 <div className='reviews-container'>
-//                     <h5 className="ratings">
-//                         <i className="fa-solid fa-star"></i>{!spot.avgStarRating ? <span>NEW</span> : spot.avgStarRating.toFixed(1)}
+//                     <h5 className='ratings'>
+//                         <i className='fa-solid fa-star'></i>{!spot.avgStarRating ? <span>NEW</span> : spot.avgStarRating.toFixed(1)}
 //                     </h5>
 //                     {spot.numReviews ? '·' : <p></p>}
-//                     <h5 className="numReviews">
+//                     <h5 className='numReviews'>
 //                         {spot.numReviews ? `${spot.numReviews} ${spot.numReviews > 1 ? 'reviews' : 'review'}` : <p></p>}
 //                     </h5>
 //                 </div>
@@ -178,9 +158,9 @@ export default SingleSpotDetails;
 //             </div>
 //             <div className='reviews-container'>
 //                 <div className='star-review-line'>
-//                     <p className='rating'><i className="fa-solid fa-star"></i>{!spot.avgStarRating ? <p>New</p> : spot.avgStarRating.toFixed(1)}
+//                     <p className='rating'><i className='fa-solid fa-star'></i>{!spot.avgStarRating ? <p>New</p> : spot.avgStarRating.toFixed(1)}
 //                     </p>
-//                     {spot.numReviews ?<i className="fa-solid fa-circle"></i> : <p></p>}
+//                     {spot.numReviews ?<i className='fa-solid fa-circle'></i> : <p></p>}
 //                     <p className='num-reviews'>
 //                     {spot.numReviews ? `${spot.numReviews} ${spot.numReviews > 1 ? 'reviews' : 'review'}` : <p></p>}
 //                     </p>
